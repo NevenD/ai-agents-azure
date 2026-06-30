d3d07536e890d0068b057b2f3a17cfc04cefd6a4
Neven Dimač <neven.dimac@gmail.com>
2026-06-30 12:22:23 +0200

    Add project files.

---
+++ src/BasicAgentApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
+
+var endpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT") ?? throw new InvalidOperationException("Environment variable 'AZURE_OPENAI_ENDPOINT' is not set");
+var model = Environment.GetEnvironmentVariable("AZURE_OPENAI_DEPLOYMENT_NAME") ?? "gpt-5-mini";
+
+// Create an IChatClient using the Azure OpenAI client with Azure CLI credentials
+IChatClient chatClient = new AzureOpenAIClient(
+    new Uri(endpoint),
+    new AzureCliCredential())
+    .GetChatClient(model)
+    .AsIChatClient();
+// Define the Agent anatomy and instructions
+
+AIAgent supportAgent = chatClient.AsAIAgent(
+    name: "SupportAgent",
+    instructions: "You are a Tier 1 IT support Agent. Your answers must be concise, professional and limited strictuly to network issues. " +
+    "Any other queries should be politly declined. focus only on network issues related to the IT.");
+
+
+Console.WriteLine($"Agent {supportAgent.Name} is online. \n");
+
+string userIssue = "I am getting a DNS resolution error when connecting to the corporate VPN from a coffee shop.";
+Console.WriteLine($"User Issue: {userIssue}");
+
+AgentResponse response = await supportAgent.RunAsync(userIssue);
+Console.WriteLine($"Agent: {response.Text}");
---
+++ src/HelloAgent/Program.cs
@@ -0,0 +1,21 @@
+﻿using Azure.AI.OpenAI;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using OpenAI.Chat;
+
+var endpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT") ?? throw new InvalidOperationException("Environment variable 'AZURE_OPENAI_ENDPOINT' is not set");
+var model = Environment.GetEnvironmentVariable("AZURE_OPENAI_DEPLOYMENT_NAME") ?? "gpt-5-mini";
+//var apiKey = Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY") ?? throw new InvalidOperationException("Environment variable 'AZURE_OPENAI_API_KEY' is not set");
+
+// Create an AI agent using the Azure OpenAI client with Azure CLI credentials
+AIAgent agent = new AzureOpenAIClient(new Uri(endpoint), new AzureCliCredential())
+    .GetChatClient(model)
+    .AsAIAgent(instructions: "You are a helpful assistant. Keep answers brief, 2 sentences max");
+
+// Create an AI agent using the Azure OpenAI client with an API key it is not recommended to use this method in production,
+// use AzureCliCredential instead
+//AIAgent agent = new AzureOpenAIClient(new Uri(endpoint), new ApiKeyCredential(apiKey))
+//    .GetChatClient(model)
+//    .AsAIAgent(instructions: "You are a helpful assistant. Keep answers brief.");
+
+Console.WriteLine(await agent.RunAsync("What is the capital of Croatia?"));
